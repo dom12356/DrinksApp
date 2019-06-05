40da1c13f6f505ce8e41fe80698eb990c60ce194
Dominic Morris <dominicmorris@outlook.com>
2019-06-05 15:29:27 +0100

    Add project files.

---
+++ DrinksApp/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace DrinksApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
